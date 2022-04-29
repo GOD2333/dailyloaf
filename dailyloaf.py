eeee240d2fd8dbb6c416e9b3a4a83fe58ccd1dc1
GOD2333 <38485808+GOD2333@users.noreply.github.com>
2022-04-29 17:18:54 +0800

    Update dailyloaf.py

--- dailyloaf.py
+++ dailyloaf.py
@@ -1,13 +1,13 @@
 from hoshino import Service, util
 from hoshino.typing import MessageSegment, CQHttpError, CQEvent, HoshinoBot
 from hoshino import aiorequests
-sv = Service('dailynews', enable_on_default=False, help_='''每日摸鱼
+sv = Service('摸鱼人日历', enable_on_default=False, help_='''每日摸鱼
 启用后会在每天早上发送一份摸鱼日历
 [@bot 今日摸鱼] （测试用）手动发送一份早报''')
 
 
 @sv.scheduled_job('cron', hour='8', minute='40', jitter=50)
-async def autonews():
+async def autoloaf():
     try:
         info = await aiorequests.get('https://api.j4u.ink/proxy/remote/moyu.json')
         try:
@@ -15,21 +15,21 @@ async def autonews():
         except:
             print(await info.text)
             raise
-        if info['message'] == 'Success':
+        if info['message'] == 'success':
             await sv.broadcast(
-                MessageSegment.image(info['data']['moyu_url'], cache=True), 'dailynews')
+                MessageSegment.image(info['data']['moyu_url'], cache=True), 'dailyloaf')
         else:
-            sv.logger.error(f'daily news error {info["msg"]}')
+            sv.logger.error(f'daily loaf error {info["msg"]}')
     except CQHttpError as e:
-        sv.logger.error(f'daily news error {e}')
+        sv.logger.error(f'daily loaf error {e}')
         raise
 
 
 @sv.on_fullmatch('今日摸鱼', only_to_me=True)
-async def handnews(bot: HoshinoBot, ev: CQEvent):
+async def handloaf(bot: HoshinoBot, ev: CQEvent):
     info = await aiorequests.get('https://api.j4u.ink/proxy/remote/moyu.json')
     info = await info.json()
-    if info['message'] == 'Success':
+    if info['message'] == 'success':
         await bot.send(ev, MessageSegment.image(info['data']['moyu_url'], cache=True))
     else:
         sv.logger.error(f'error {info["message"]}')
