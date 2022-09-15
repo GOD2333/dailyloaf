4c27b3c1f3d934fff132f629263191a36d19c5f7
GOD2333 <38485808+GOD2333@users.noreply.github.com>
2022-09-15 15:44:16 +0800

    更新api

--- dailyloaf.py
+++ dailyloaf.py
@@ -9,7 +9,7 @@ sv = Service('摸鱼人日历', enable_on_default=False, help_='''每日摸鱼
 @sv.scheduled_job('cron', hour='8', minute='40', jitter=50)
 async def autoloaf():
     try:
-        info = await aiorequests.get('https://api.j4u.ink/proxy/remote/moyu.json')
+        info = await aiorequests.get('https://api.j4u.ink/v1/store/other/proxy/remote/moyu.json')
         try:
             info = await info.json()
         except:
@@ -27,7 +27,7 @@ async def autoloaf():
 
 @sv.on_fullmatch('今日摸鱼', only_to_me=True)
 async def handloaf(bot: HoshinoBot, ev: CQEvent):
-    info = await aiorequests.get('https://api.j4u.ink/proxy/remote/moyu.json')
+    info = await aiorequests.get('https://api.j4u.ink/v1/store/other/proxy/remote/moyu.json')
     info = await info.json()
     if info['message'] == 'success':
         await bot.send(ev, MessageSegment.image(info['data']['moyu_url'], cache=True))
